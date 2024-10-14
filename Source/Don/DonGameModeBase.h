4ecdd83a819ede81c8d194ade58f8be1199c7bcc
Fsbsnw <108972912+Fsbsnw@users.noreply.github.com>
2024-10-14 19:16:12 +0900

    Start
    Initialize Repository

---
+++ Source/Don/DonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DON_API ADonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
